f175f816f5bd03cc4829839e471bea2e22ea9145
aq2r <183514119+aq2r@users.noreply.github.com>
2025-04-11 23:27:41 +0900

    portrait, description

--- src/components/controllers/speaker.tsx
+++ src/components/controllers/speaker.tsx
@@ -1,9 +1,23 @@
 import { useAtom, useAtomValue } from "jotai"
 import { jotaiAtoms } from "../../atoms"
+import { useEffect, useState } from "react"
+import { RustInvoke } from "../../rustInvoke"
 
 export const SpeakerSelector = () => {
     const selectModel = useAtomValue(jotaiAtoms.selectModel)
     const [selectSpeakerIdx, setSelectSpeakerIdx] = useAtom(jotaiAtoms.selectSpeakerIdx)
+
+    const [imageSrc, setImageSrc] = useState("");
+
+    useEffect(() => {
+        const loadImage = async () => {
+            const base64Image = await RustInvoke.otherReadImage(
+                `${selectModel?.model_path}/${selectModel?.voices[selectSpeakerIdx].portrait_path}`
+            );
+            setImageSrc(base64Image);
+        }
+        loadImage();
+    }, [selectModel, selectSpeakerIdx]);
 
     return (
         <>
@@ -17,9 +31,20 @@ export const SpeakerSelector = () => {
                     {selectModel?.voices.map((i, idx) => { return <option key={i.name} value={idx}>{i.name}</option> })}
                 </select>
 
+                <div className="speaker-portrait">
+                    <img src={imageSrc}></img>
+                </div>
+
+                <div className="speaker-portrait-description">
+                    画像の説明:{`\n${selectModel?.voices[selectSpeakerIdx].portrait_description}`}
+
+                </div>
+
                 <div className="description">
                     <label>{selectModel?.voices[selectSpeakerIdx].description}</label>
                 </div>
+
+
             </div>
         </>)
 }
